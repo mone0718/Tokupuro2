9040c5f04019002005ea0ebd4ae0e47c9fb2fa11
mone0718 <monefujita.eb@keio.jp>
2022-11-25 20:53:29 +0900

    Remove %

--- EMGvsForce.m
+++ EMGvsForce.m
@@ -16,97 +16,97 @@ Force_list = [20,40,60,80,100];
 iEMG_list = [0,0,0,0,0];
 
 % %20%,40%,60%,80%,MVCの順で読み込む(ために5回繰り返し)
-% for i = 1:5
-% 
-%     %サンプリング周波数
-%     fs = 1000;
-% 
-%     %解析するデータ（matファイル）を選択し、読み込む
-%     [fname,pname] = uigetfile('*.mat','解析するデータを選択してください');
-%     FP = [fname pname];
-%     if fname == 0;return;end
-%     %fnameがファイル名／pnameはファイルのある場所（ディレクトリ）
-%     load([pname fname]);
-% 
-%     %フィルタリング
-%     data_filtered = data;
-%     %ハムカットフィルタ:地域周波数(50Hz)の倍数をフィルタリング
-%     %なんで倍数？
-%     %ローパスを500Hzに設定してる(500以上はカットしてる)から450まで
-%     [b50,a50] = butter(3,[49 51]/500,'stop');
-%     [b100,a100] = butter(3,[99 101]/500,'stop');
-%     [b150,a150] = butter(3,[149 151]/500,'stop');
-%     [b200,a200] = butter(3,[199 201]/500,'stop');
-%     [b250,a250] = butter(3,[249 251]/500,'stop');
-%     [b300,a300] = butter(3,[299 301]/500,'stop');
-%     [b350,a350] = butter(3,[349 351]/500,'stop');
-%     [b400,a400] = butter(3,[399 401]/500,'stop');
-%     [b450,a450] = butter(3,[449 451]/500,'stop');
-%     data_filtered = filtfilt(b50,a50,data_filtered);
-%     data_filtered = filtfilt(b100,a100,data_filtered);
-%     data_filtered = filtfilt(b150,a150,data_filtered);
-%     data_filtered = filtfilt(b200,a200,data_filtered);
-%     data_filtered = filtfilt(b250,a250,data_filtered);
-%     data_filtered = filtfilt(b300,a300,data_filtered);
-%     data_filtered = filtfilt(b350,a350,data_filtered);
-%     data_filtered = filtfilt(b400,a400,data_filtered);
-%     data_filtered = filtfilt(b450,a450,data_filtered);
-% 
-%     %計測データの定義
-%     %リスト名(行,列) リストの要素を指定.指定しない(全選択)時は:.
-%     Force = data_filtered(:,1); %1列目のデータ
-%     EMG = data_filtered(:,2); %2列目のデータ
-% 
-%     %EMGは1000μV→1Vなので、マイクロボルト単位に変換（1000倍)
-%     %基線ズレがある可能性があるので平均を引く
-%     EMG = (EMG-mean(EMG))*1000;
-% 
-%     %全波整流（絶対値化）
-%     rEMG = abs(EMG);
-% 
-%     %時間行列を作成
-%     time = 0:1/fs:length(Force)/fs-1/fs;
-% 
-%     %生波形を描画
-%     figure('Position',[1 1 500 700]);
-%     subplot(2,1,1);
-%     plot(time,Force);
-%     ylabel('Force (V)','FontName','Arial','Fontsize',12);
-%     xlabel('time (s)','FontName','Arial','Fontsize',12);
-% 
-%     subplot(2,1,2);
-%     plot(time,rEMG);
-%     ylabel('rEMG (\muV)','FontName','Arial','Fontsize',12);
-%     xlabel('time (s)','FontName','Arial','Fontsize',12);
-% 
-%     uiwait; %何かのアクションがあるまでプログラムがストップ
-% 
-% 
-%     %解析対象区間を設定
-%     %安定した2秒間のデータを計算
-%     defaultanswer = {'12','14'};
-%     startend = inputdlg({'start','end'},'解析区間の2秒を設定してください',1,defaultanswer);
-%     start_time = str2num(char(startend(1)));
-%     end_time = str2num(char(startend(2)));
-% 
-%     %解析対象区間の2秒分のデータを切り出し
-%     Force = Force(start_time*fs+1:end_time*fs);
-%     rEMG = rEMG(start_time*fs+1:end_time*fs);
-% 
-%     %2秒間の積分振幅を計測
-%     iEMG = sum(rEMG);
-% 
-%     iEMG_list(5) = iEMG;
-% 
-%     if subject_name == "Egashira"
-%         jonah_iEMG_list(5) = iEMG;
-%     elseif subject_name == "Takeuchi"
-%             takeuchi_iEMG_list(5) = iEMG;
-%     elseif subject_name == "Yokota"
-%             yokota_iEMG_list(5) = iEMG;
-%     end
-% 
-% end
+for i = 1:5
+
+    %サンプリング周波数
+    fs = 1000;
+
+    %解析するデータ（matファイル）を選択し、読み込む
+    [fname,pname] = uigetfile('*.mat','解析するデータを選択してください');
+    FP = [fname pname];
+    if fname == 0;return;end
+    %fnameがファイル名／pnameはファイルのある場所（ディレクトリ）
+    load([pname fname]);
+
+    %フィルタリング
+    data_filtered = data;
+    %ハムカットフィルタ:地域周波数(50Hz)の倍数をフィルタリング
+    %なんで倍数？
+    %ローパスを500Hzに設定してる(500以上はカットしてる)から450まで
+    [b50,a50] = butter(3,[49 51]/500,'stop');
+    [b100,a100] = butter(3,[99 101]/500,'stop');
+    [b150,a150] = butter(3,[149 151]/500,'stop');
+    [b200,a200] = butter(3,[199 201]/500,'stop');
+    [b250,a250] = butter(3,[249 251]/500,'stop');
+    [b300,a300] = butter(3,[299 301]/500,'stop');
+    [b350,a350] = butter(3,[349 351]/500,'stop');
+    [b400,a400] = butter(3,[399 401]/500,'stop');
+    [b450,a450] = butter(3,[449 451]/500,'stop');
+    data_filtered = filtfilt(b50,a50,data_filtered);
+    data_filtered = filtfilt(b100,a100,data_filtered);
+    data_filtered = filtfilt(b150,a150,data_filtered);
+    data_filtered = filtfilt(b200,a200,data_filtered);
+    data_filtered = filtfilt(b250,a250,data_filtered);
+    data_filtered = filtfilt(b300,a300,data_filtered);
+    data_filtered = filtfilt(b350,a350,data_filtered);
+    data_filtered = filtfilt(b400,a400,data_filtered);
+    data_filtered = filtfilt(b450,a450,data_filtered);
+
+    %計測データの定義
+    %リスト名(行,列) リストの要素を指定.指定しない(全選択)時は:.
+    Force = data_filtered(:,1); %1列目のデータ
+    EMG = data_filtered(:,2); %2列目のデータ
+
+    %EMGは1000μV→1Vなので、マイクロボルト単位に変換（1000倍)
+    %基線ズレがある可能性があるので平均を引く
+    EMG = (EMG-mean(EMG))*1000;
+
+    %全波整流（絶対値化）
+    rEMG = abs(EMG);
+
+    %時間行列を作成
+    time = 0:1/fs:length(Force)/fs-1/fs;
+
+    %生波形を描画
+    figure('Position',[1 1 500 700]);
+    subplot(2,1,1);
+    plot(time,Force);
+    ylabel('Force (V)','FontName','Arial','Fontsize',12);
+    xlabel('time (s)','FontName','Arial','Fontsize',12);
+
+    subplot(2,1,2);
+    plot(time,rEMG);
+    ylabel('rEMG (\muV)','FontName','Arial','Fontsize',12);
+    xlabel('time (s)','FontName','Arial','Fontsize',12);
+
+    uiwait; %何かのアクションがあるまでプログラムがストップ
+
+
+    %解析対象区間を設定
+    %安定した2秒間のデータを計算
+    defaultanswer = {'12','14'};
+    startend = inputdlg({'start','end'},'解析区間の2秒を設定してください',1,defaultanswer);
+    start_time = str2num(char(startend(1)));
+    end_time = str2num(char(startend(2)));
+
+    %解析対象区間の2秒分のデータを切り出し
+    Force = Force(start_time*fs+1:end_time*fs);
+    rEMG = rEMG(start_time*fs+1:end_time*fs);
+
+    %2秒間の積分振幅を計測
+    iEMG = sum(rEMG);
+
+    iEMG_list(5) = iEMG;
+
+    if subject_name == "Egashira"
+        jonah_iEMG_list(5) = iEMG;
+    elseif subject_name == "Takeuchi"
+            takeuchi_iEMG_list(5) = iEMG;
+    elseif subject_name == "Yokota"
+            yokota_iEMG_list(5) = iEMG;
+    end
+
+end
 
 %リストの情報を保存
 output_filename = sprintf('%s_EMGvsForce',subject_name);
